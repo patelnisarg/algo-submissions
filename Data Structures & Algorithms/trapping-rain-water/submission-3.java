91ba64b774cc07cd769bdbacca1908c023ac3c4e
Nisarg Patel <34495080+patelnisarg@users.noreply.github.com>
2026-06-29 15:08:12 -0700

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.java
@@ -0,0 +1,28 @@
+/**
+- at every point if we know the leftMax, rightMax we can caluculate how much of the water 
+can be trapped at the current index 
+- create a leftMax array and rightMax array and than use that to calculate
+*/
+class Solution {
+    public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int lMax = -1, rMax = -1;
+        for(int i = 0; i < height.length; i++){
+            lMax = Math.max(lMax, height[i]);
+            leftMax[i] = lMax;
+        }
+        for(int i = height.length-1; i >= 0; i--){
+            rMax = Math.max(rMax, height[i]);
+            rightMax[i] = rMax;
+        }
+        int trappedWater = 0;
+        for(int i = 0; i < height.length; i++){
+            int currTrapped = Math.min(leftMax[i], rightMax[i]) - height[i];
+            if(currTrapped <= 0) continue;
+            trappedWater += currTrapped;
+        }
+
+        return trappedWater;
+    }
+}
